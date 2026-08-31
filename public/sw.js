f03bb490475b8542ea6f24df471312ba223f223f
winston0510 <sunyuewei8023@gmail.com>
2026-09-01 07:58:00 +0800

    v4.5.4: sw cache + APP_VERSION

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kaoyan-v4.5.3';
+const CACHE = 'kaoyan-v4.5.4';
 const PRECACHE = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon.svg'];
 
 self.addEventListener('install', (event) => {

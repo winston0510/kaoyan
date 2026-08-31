c06d1959168f2642700884cc655ad6bb5b08dfce
winston0510 <sunyuewei8023@gmail.com>
2026-08-31 20:09:47 +0800

    v4.3.0: iOS glassmorphism UI - sw cache & version bump

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kaoyan-v4.2.1';
+const CACHE = 'kaoyan-v4.3.0';
 const PRECACHE = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon.svg'];
 
 self.addEventListener('install', (event) => {

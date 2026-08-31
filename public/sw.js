c17eff3a68825e573a82fd597c15993faf357758
winston0510 <sunyuewei8023@gmail.com>
2026-08-31 22:17:36 +0800

    v4.5.1: sw 缓存 kaoyan-v4.5.1 + APP_VERSION v4.5.1 + manifest theme_color #FFFFFF

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kaoyan-v4.5.0';
+const CACHE = 'kaoyan-v4.5.1';
 const PRECACHE = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon.svg'];
 
 self.addEventListener('install', (event) => {

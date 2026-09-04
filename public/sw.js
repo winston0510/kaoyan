715e057720ef0373141b1951e732f9e837a3cced
winston0510 <sunyuewei8023@gmail.com>
2026-09-05 06:07:46 +0800

    v4.8.1 知识主题改为清华孝哥公开资料原文录取（knowledge-data / constants / sw）

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kaoyan-v4.8.0';
+const CACHE = 'kaoyan-v4.8.1';
 const PRECACHE = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon.svg'];
 
 self.addEventListener('install', (event) => {

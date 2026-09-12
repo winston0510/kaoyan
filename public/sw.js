c37b24f1b8da19ef7752d9c9b5e4fa57467e614c
winston0510 <sunyuewei8023@gmail.com>
2026-09-12 14:48:05 +0800

    v4.9.2: 政治/电路章节目录合并规范（+6 兜底章、电路补 2 章）+ 缓存名升版

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kaoyan-v4.9.0';
+const CACHE = 'kaoyan-v4.9.2';
 const PRECACHE = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon.svg'];
 
 self.addEventListener('install', (event) => {

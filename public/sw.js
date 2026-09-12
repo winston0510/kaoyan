9852703c38280e51d54cb2c2687387184577ea02
winston0510 <sunyuewei8023@gmail.com>
2026-09-12 20:10:06 +0800

    feat(v4.11.0): 政治目录换代到 2023 版教材章名（64 条目），同步升级 sw 缓存名

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kaoyan-v4.10.1';
+const CACHE = 'kaoyan-v4.11.0';
 const PRECACHE = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon.svg'];
 
 self.addEventListener('install', (event) => {

37b0a126d1feed418d61e48563256d8f6acdf4eb
winston0510 <sunyuewei8023@gmail.com>
2026-09-12 16:58:21 +0800

    v4.10.0：缓存名升至 kaoyan-v4.10.0（套卷/计划/背诵模式）

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kaoyan-v4.9.2';
+const CACHE = 'kaoyan-v4.10.0';
 const PRECACHE = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon.svg'];
 
 self.addEventListener('install', (event) => {
